a05ff7950af3e239f8c51877e57c562bc50f7618
clara <clara@minneapolis.edu>
2019-09-13 19:12:31 -0500

    leaflet map updates, comments

--- leaflet_maps/maps.js
+++ leaflet_maps/maps.js
@@ -1,19 +1,18 @@
-// Array with latitude and longitude 
-let metroAreaCenterCoordinates = [44.96, -93.2]
+let metroAreaCenterCoordinates = [44.96, -93.2]  // Array of latitude and longitude 
+let zoomLevel = 9   // 1 = whole world, 10 = large city, 20 = city blocks
 
 // Create the map 
-let map = L.map('college-map').setView(metroAreaCenterCoordinates, 9)
+let map = L.map('college-map').setView(metroAreaCenterCoordinates, zoomLevel)
 
 // Add the tile layer - roads, streets etc. Without this, nothing to see 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy;  <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiY2xhcmFsIiwiYSI6ImNqcmdwenViYTAwcHQ0Ym5yYmZ1Z3E2bjgifQ.QQfUvVaqPsWb_jJbP2gvHg'
 }).addTo(map)
 
 
-// Add some markers 
+// Add some markers for Minneapolis College and Saint Paul College
 let mctcCoordinates = [44.9724, -93.2844]
 let mctcMarker = L.marker(mctcCoordinates)
     .bindPopup("Minnepolis College<br><a href='https://minneapolis.edu'>Website</a>")
@@ -25,13 +24,12 @@ let stpMarker = L.marker(stPaulCoordinates)
     .addTo(map)
 
 
-// Add a circle 
-var metroAreaCircle = L.circle(metroAreaCenterCoordinates, {
+// Add a circle approximately around the Twin Cities metro 
+let metroAreaCircle = L.circle(metroAreaCenterCoordinates, {
         color: 'green',  
         radius: 30000,
         fillOpacity: 0.2
     })
     .bindPopup("Twin Cities Metro Area")
     .addTo(map)
-
 
