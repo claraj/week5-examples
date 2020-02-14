ee95d85be3a571a634894d93578d842c1ecbda6a
clara <qwerty@sdhsdg>
2020-02-13 19:01:40 -0600

    fix typo, replace quotes

--- canvas/draw_on_a_picture/draw_word.js
+++ canvas/draw_on_a_picture/draw_word.js
@@ -1,31 +1,32 @@
 //Find canvas, create a context
-let canvas = document.querySelector("#canvas");
-let context = canvas.getContext("2d");
+let canvas = document.querySelector('#canvas')
+let context = canvas.getContext('2d')
 
 //Find input box and disable it until canvas is ready
-let input = document.querySelector("#image_text");
-input.disabled = true;
+let input = document.querySelector('#image_text')
+input.disabled = true
 
 // Drawing the image - create an image and set the source 
 // add callback to know when the image is loaded and ready, 
 // so can draw it to the canvas and enable the input
-let image = new Image();
-image.src = "cat.jpg";
+let image = new Image()
+image.src = 'cat.jpg'
 
-image.addEventListener("load", function () {
-    context.drawImage(image, 0, 0);
-    input.disabled = false;
+image.addEventListener('load', function () {
+    context.drawImage(image, 0, 0)
+    input.disabled = false
 })
 
 // Add event listener to input - will fire if the text is changed
-input.addEventListener("input", function() {
+input.addEventListener('input', function() {
     // Read the data typed in the input
     // 'this' is the HTML element this function is called for 
-    let text = this.value;
+    let text = this.value
 
-    context.drawImage(image, 0, 0);     // Need to redraw the image - no way to clear 
-    context.font = "30px Arial Black";  // Set font
-    context.fillText(text, 30, 100);    // And draw text.
-});
+    context.drawImage(image, 0, 0)     // Need to redraw the image - no way to clear 
+    context.font = '30px Arial'  // Set font
+    context.fillStyle = 'Black'
+    context.fillText(text, 30, 100)    // And draw text.
+})
 
 
